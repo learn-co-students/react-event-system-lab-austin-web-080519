c22ef0827e8a5360a5041564947871b1a02dab41
Syd <green@Syds-MacBook-Air-2.local>
2019-10-08 14:04:22 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    focusAction = () => {
+        console.log('Good!')
+    }
+
+    blurAction = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    render() {
+        return (
+            <button 
+            onFocus={this.focusAction}
+            onBlur={this.blurAction}
+            />
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Keypad from './components/Keypad';
+import Keypad from './components/Keypad.js';
 import EyesOnMe from './components/EyesOnMe';
 
 ReactDOM.render(
